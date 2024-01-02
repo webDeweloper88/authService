fa27cf2eb38b6369da9179aaf2d46454f47de703
Mr.Makhmud <m.xudoyberdiev88@gmail.com>
2024-01-02 08:45:08 +0500

    initAuthService

---
+++ src/common/constans/errors.ts
@@ -0,0 +1,5 @@
+export const AppError = {
+  USER_EXIST: 'Bu emailga ega foydalanuvchi allaqachon mavjud',
+  USER_NOT_EXIST: 'Bunday foydalanuvchi mavjud emas ',
+  WRONG_DATA: 'Login yoki parolingizda xatolik',
+};
